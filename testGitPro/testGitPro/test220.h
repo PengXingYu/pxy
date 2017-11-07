0c20c0f890deb8dc6e37d5dc6b4a310ec1ff92b4
徐志鹏 <1049748052@qq.com>
2017-11-07 22:40:41 +0800

    增加testObj220文件

---
+++ testGitPro/testGitPro/test220.h
@@ -0,0 +1,13 @@
+//
+//  test220.h
+//  testGitPro
+//
+//  Created by 徐志鹏 on 2017/11/7.
+//  Copyright © 2017年 徐志鹏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test220 : NSObject
+
+@end
---
+++ testGitPro/testGitPro/test220.m
@@ -0,0 +1,13 @@
+//
+//  test220.m
+//  testGitPro
+//
+//  Created by 徐志鹏 on 2017/11/7.
+//  Copyright © 2017年 徐志鹏. All rights reserved.
+//
+
+#import "test220.h"
+
+@implementation test220
+
+@end
