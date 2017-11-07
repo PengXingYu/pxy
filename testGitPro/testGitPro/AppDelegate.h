7ab3bf6e69c3f48d9a1a8d06e4c9629eadd83e9a
徐志鹏 <1049748052@qq.com>
2017-11-07 20:43:27 +0800

    新加入一个项目

---
+++ testGitPro/testGitPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGitPro
+//
+//  Created by 徐志鹏 on 2017/11/7.
+//  Copyright © 2017年 徐志鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGitPro/testGitPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGitPro
+//
+//  Created by 徐志鹏 on 2017/11/7.
+//  Copyright © 2017年 徐志鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
